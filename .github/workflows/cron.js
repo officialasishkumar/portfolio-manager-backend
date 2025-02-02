c931577d9c3898dba9a3302804fe2b90908d58f1
Asish Kumar <officialasishkumar@gmail.com>
2025-02-02 20:40:15 +0530

    keep alive
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>

---
+++ .github/workflows/cron.js
@@ -0,0 +1,18 @@
+
+const https = require('https');
+
+const backendUrl = 'https://portfolio-manager-backend-hwg1.onrender.com';
+
+console.log('Attempting to keep server alive...');
+
+https
+    .get(backendUrl, (res) => {
+        if (res.statusCode === 200) {
+            console.log('Server is alive');
+        } else {
+            console.error(`Failed with status code: ${res.statusCode}`);
+        }
+    })
+    .on('error', (err) => {
+        console.error('Error occurred:', err.message);
+    });
